bc9e498ec80b998a7d0979ce8a14f24645cdd2c6
Mariya <meari.kachusova@mail.ru>
2019-08-02 18:30:19 +0300

    add personal page and all users page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'dashboard#index'
+  get '/users', to: 'users#index', as: :all_users
+  get '/users/:id', to: 'users#show', as: :show_user
 end
