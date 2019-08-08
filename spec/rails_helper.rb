5b92c21534aad381411e91594fa291424a3fd9ed
Mariya <meari.kachusova@mail.ru>
2019-08-09 02:48:31 +0300

    add chrome to travis

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -111,9 +111,6 @@ RSpec.configure do |config|
     DatabaseCleaner.clean
   end
 end
-Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
-end
 
 Capybara.register_driver :headless_chrome do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
@@ -125,6 +122,12 @@ end
 
 Capybara.register_driver :selenium do |app|
   Capybara::Selenium::Driver.new(app, :browser => :chrome)
+end
+
+Capybara.register_driver :chrome do |app|
+  options = Selenium::WebDriver::Chrome::Options.new(args: %w[no-sandbox headless disable-gpu])
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
 
 Capybara.javascript_driver = :headless_chrome
