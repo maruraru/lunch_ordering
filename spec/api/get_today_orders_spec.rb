8a7e0730128903b64e77a349cc55abd5f683b30c
Mariya <meari.kachusova@mail.ru>
2019-08-08 17:08:49 +0300

    add some security to api

--- spec/api/get_today_orders_spec.rb
+++ spec/api/get_today_orders_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'Orders API', type: :request do
+  before :each do
+    admin = FactoryBot.create(:user)
+    @token = admin.authentication_token
+  end
 
   context 'when orders are ready' do
     before do
@@ -12,19 +16,30 @@ RSpec.describe 'Orders API', type: :request do
           FactoryBot.create(:user_lunch, user: user, menu_item: item)
         end
       end
-      get '/api/v1/user_orders'
+      get '/api/v1/user_orders', headers: { 'X-USER-TOKEN' => @token }
     end
-    it 'returns orders' do
+
+    it 'returns status code 200' do
       expect(response).to have_http_status(200)
+    end
+
+    it 'returns orders details' do
       parsed_body = JSON.parse(response.body)
       expect(parsed_body[0].keys).to match_array(%w[category name price user_name])
     end
   end
 
   context 'when orders are empty' do
-    it 'returns empty data' do
-      get '/api/v1/user_orders'
+    it 'returns status code 204' do
+      get '/api/v1/user_orders', headers: { 'X-USER-TOKEN' => @token }
       expect(response).to have_http_status(204)
+    end
+  end
+
+  context 'when user token is absent' do
+    it 'returns status code 401' do
+      get '/api/v1/user_orders'
+      expect(response).to have_http_status(401)
     end
   end
 end
